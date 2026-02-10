89d48bd91923b52525ade9369755ff323e19cda5
DigesSuvagiya <suvagiyadigesh1284@gmail.com>
2026-02-10 16:56:27 +0530

    .

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,23 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://bsp-backend-femb.onrender.com/api",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default api;
-
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://bsp-backend-femb.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default api;
+
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
 
